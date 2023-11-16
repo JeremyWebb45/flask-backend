47f6527589ea3c18308ee0bdcb5fbe46de769c63
JeremyWebb45 <jeremy.webb429@gmail.com>
2023-11-15 21:30:25 -0700

    simple server

---
+++ main.py
@@ -0,0 +1,22 @@
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return render_template("about.html")
+
+@app.route("/data-science")
+def dataScience():
+    return render_template("data-science.html")
+
+@app.route("/front-end")
+def frontEnd():
+    return render_template("front-end.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ static/components/header/header.js
@@ -0,0 +1,25 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav>
+          <a href="about.html">
+            <img src="static/favicon.ico" alt="logo icon" width="32" height="32"/>
+            <p>ebbfolio</p>
+          </a>
+          <ul>
+            <li><a href="data-science.html"><p>Data science</p></a></li>
+            <li><a href="front-end.html"><p>Front end</p></a></li>
+          </ul>
+          <span />
+        </nav>
+      </header>
+    `;
+  }
+}
+
+customElements.define("header-component", Header);

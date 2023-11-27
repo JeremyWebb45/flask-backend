a4800cf6ba6c8cf195ae939dabeee1ae15234d91
JeremyWebb45 <jeremy.webb429@gmail.com>
2023-11-26 19:47:07 -0700

    griddy

--- static/components/cellular-grid/cellular-grid.js
+++ static/components/cellular-grid/cellular-grid.js
@@ -2,29 +2,29 @@ class CellularGrid extends HTMLElement {
   grid;
   height;
   width;
+  tBody;
   constructor() {
     super();
     const serverData = JSON.parse(this.getAttribute("data-"));
     this.grid = serverData.grid;
     this.height = serverData.height;
     this.width = serverData.width;
+    this.tBody = document.getElementById("gridBody");
+    console.log(this.tBody);
   }
 
   connectedCallback() {
-    const gridContainer = document.createElement("table");
-    const tBody = gridContainer.createTBody();
+    const gridContents = document.createElement("div");
     this.grid.forEach((row, rowIdx) => {
       const currRow = document.createElement("tr");
-      tBody.appendChild(currRow);
+      gridContents.appendChild(currRow);
       row.forEach((cell, cellIdx) => {
         const currCell = document.createElement("td");
-        currCell.addEventListener("click", () => {
-          console.log("hey");
-        });
         currRow.appendChild(currCell);
       });
     });
-    this.innerHTML = gridContainer.outerHTML;
+    console.log(gridContents.innerHTML);
+    this.innerHTML = gridContents.innerHTML;
   }
 }
 
